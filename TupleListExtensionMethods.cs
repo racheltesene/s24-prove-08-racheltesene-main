23254d91689e0ddce6ca1a07a8b0f0fff475fa51
racheltesene <tes21002@byui.edu>
2024-06-15 13:05:37 -0600

    first commit

---
+++ TupleListExtensionMethods.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+
+namespace prove_08;
+
+public static class TupleListExtensionMethods {
+    public static string AsString(this IEnumerable list) {
+        return "<List>{" + string.Join(", ", list.Cast<ValueTuple<int, int>>()) + "}";
+    }
+}
